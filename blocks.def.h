3d4910006a91aa13d55499029223af9a7e4fa6ea
Riicarus <riicarus.acc@gmail.com>
2024-02-12 23:05:37 +0800

    add: cpu & date & mem & wlan

--- blocks.def.h
+++ blocks.def.h
@@ -1,8 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{ " ", "~/scripts/cpu.sh",		2,			0 },	/* Set first block's icon 1 char padding */
-	{ "󱑌", "~/scripts/date.sh",		1,			0 },
+	{ " ", 	"~/scripts/cpu.sh",	2,			0 },	/* Set first block's icon 1 char padding */
+	{ "󰰑", 		"~/scripts/mem.sh",	1,			0 },
+	{ "", 		"~/scripts/wlan.sh",	1,			0 },
+	{ "󱑌", 		"~/scripts/date.sh",	1,			0 },
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
---
+++ scripts/wlan.sh
@@ -0,0 +1,43 @@
+function get_bytes {
+    default_route=$(ip route | grep default)
+    interface=$(echo "$default_route" | awk '{print $5}')
+    received_bytes=$(cat /sys/class/net/$interface/statistics/rx_bytes)
+    transmitted_bytes=$(cat /sys/class/net/$interface/statistics/tx_bytes)
+    line=$(echo $received_bytes $transmitted_bytes | awk '{print "received_bytes="$1, "transmitted_bytes="$2}')
+    eval $line
+    now=$(date +%s%N)
+}
+
+
+function get_velocity {
+    value=$1
+    old_value=$2
+    now=$3
+    timediff=$(($now - $old_time))
+    velB=$(echo $value $old_value $timediff | awk '{printf "%.0f", 1000000000*($1-$2)/$3}')
+    if test "$velB" -gt 1024
+    then
+	if test "$(($velB/1024))" -gt 1024
+	then
+	    echo $(echo $velB | awk '{printf "%.1f", $1/1024/1024}')MB/s
+        else
+	    echo $(echo $velB | awk '{printf "%.1f", $1/1024}')KB/s
+	fi
+    else
+        echo ${velB}B/s
+    fi
+}
+
+get_bytes
+old_received_bytes=$received_bytes
+old_transmitted_bytes=$transmitted_bytes
+old_time=$now
+
+sleep 1s
+
+get_bytes
+
+vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
+vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
+
+echo " $vel_recv⬇ $vel_trans⬆"
