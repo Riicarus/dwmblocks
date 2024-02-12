cb19be24c4aa111855bc2fd1dad25eb402a8bdbc
Riicarus <riicarus.acc@gmail.com>
2024-02-12 19:56:59 +0800

    update: time & date

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{ " ", "~/scripts/cpu.sh",		2,			0 },	/* Set first block's icon 1 char padding */
+	{ "󱑌", "~/scripts/date.sh",		1,			0 },
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
