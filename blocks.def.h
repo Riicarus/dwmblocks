e58183b72aa7b51245ca18d59156638e489744f9
Riicarus <riicarus.acc@gmail.com>
2024-02-12 23:16:35 +0800

    update: sh path

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{ " ", 	"~/scripts/cpu.sh",	2,			0 },	/* Set first block's icon 1 char padding */
-	{ "󰰑", 		"~/scripts/mem.sh",	1,			0 },
-	{ "", 		"~/scripts/wlan.sh",	1,			0 },
-	{ "󱑌", 		"~/scripts/date.sh",	1,			0 },
+	{ " ", 	"~/scripts/blocks/cpu.sh",	2,			0 },	/* Set first block's icon 1 char padding */
+	{ "󰰑", 		"~/scripts/blocks/mem.sh",	1,			0 },
+	{ "", 		"~/scripts/blocks/wlan.sh",	1,			0 },
+	{ "󱑌", 		"~/scripts/blocks/date.sh",	1,			0 },
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
