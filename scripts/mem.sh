7512dd285e296faefc5e1236a9007dee809ff945
Riicarus <riicarus.acc@gmail.com>
2024-02-14 23:34:01 +0800

    fix: mem.sh shows wrong percentage result

--- scripts/mem.sh
+++ scripts/mem.sh
@@ -1,5 +1,5 @@
 MEM_FREE=$(cat /proc/meminfo | grep -w MemAvailable | awk '{print $2}')
 MEM_TOTAL=$(cat /proc/meminfo | grep -w MemTotal | awk '{print $2}')
 
-MEM_USAGE=$(echo $MEM_FREE $MEM_TOTAL | awk '{printf "%.2f", $1/$2}')
+MEM_USAGE=$(echo $MEM_FREE $MEM_TOTAL | awk '{printf "%.2f", 100 * $1 / $2}')
 echo " $MEM_USAGE%"
